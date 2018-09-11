547dc6f52bfca52592421157142988b024b4b72a
ayad <ayad@eng.misuratau.edu.ly>
2018-09-11 09:56:24 +0200

    - design activity_launcher.xml - add landscape to main_activity.xml - add SelectGameActivity

--- app/src/main/java/ly/rqmana/basmaty/MainActivity.java
+++ app/src/main/java/ly/rqmana/basmaty/MainActivity.java
@@ -1,7 +1,9 @@
 package ly.rqmana.basmaty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+    }
 
-
+    public void letsStart(View view) {
+        startActivity(new Intent(this, SelectGameActivity.class));
+        finish();
     }
 }
