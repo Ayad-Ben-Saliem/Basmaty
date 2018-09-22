6ab12f114584ec53cbcc24be061aaa6ddf4f74a8
MY-NAME <a.altabbal@it.misuratau.edu.com>
2018-09-22 09:08:25 +0300

    edit the game background

--- app/src/main/java/ly/rqmana/basmaty/SelectGameActivity.java
+++ app/src/main/java/ly/rqmana/basmaty/SelectGameActivity.java
@@ -6,30 +6,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.widget.AppCompatImageView;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class SelectGameActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_select_game);
-
-    }
-
-    public void onGameClicked(View view) {
-        switch (view.getId()) {
-            case R.id.mentalGameIV:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
-            case R.id.trueFalseGameIV:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
-            case R.id.educationalGameIV:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
-        }
     }
 }
